79f04347955de19e3f7a8d4e29ea686eea158034
Korosi Zsombor <korosizsombor@gmail.com>
2017-11-09 09:15:34 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace interview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataSet1 = new System.Data.DataSet();
+            this.first_name_txt = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.last_name_txt = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // first_name_txt
+            // 
+            this.first_name_txt.Location = new System.Drawing.Point(247, 84);
+            this.first_name_txt.Name = "first_name_txt";
+            this.first_name_txt.Size = new System.Drawing.Size(198, 22);
+            this.first_name_txt.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(38, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(463, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last Name";
+            // 
+            // last_name_txt
+            // 
+            this.last_name_txt.Location = new System.Drawing.Point(466, 85);
+            this.last_name_txt.Name = "last_name_txt";
+            this.last_name_txt.Size = new System.Drawing.Size(213, 22);
+            this.last_name_txt.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(89, 151);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(521, 276);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_delete.Location = new System.Drawing.Point(651, 164);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(133, 67);
+            this.btn_delete.TabIndex = 7;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(885, 474);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.last_name_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.first_name_txt);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.TextBox first_name_txt;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox last_name_txt;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+
+namespace interview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DBconnection();
+            fill_listbox();
+        }
+        void fill_listbox()
+        {
+            string ConnectString = "Server=localhost;Database=felveteli;Uid=root;Pwd=;";
+            string Query = "select * from people";
+            MySqlConnection DBconnect = new MySqlConnection(ConnectString);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, DBconnect);
+            MySqlDataReader myReader;
+            try
+            {
+                DBconnect.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                while (myReader.Read())
+                {
+                    string nr = myReader.GetString("nr");
+                    string firstname = myReader.GetString("first_name");
+                    string lastname = myReader.GetString("last_name");
+                    listBox1.Items.Add(nr + " " + firstname + " " + lastname);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void DBconnection()
+        {
+            string ConnectString = "Server=localhost;Database=felveteli;Uid=root;Pwd=;";
+            MySqlConnection DBconnect = new MySqlConnection(ConnectString);
+            try
+            {
+                DBconnect.Open();
+                btn_delete.Visible = false;
+                btn_delete.Visible = false;
+                //MessageBox.Show("You are connected");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DBconnection();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ConnectString = "Server=localhost;Database=felveteli;Uid=root;Pwd=;";
+            string Query = "insert into people(first_name,last_name) values('"+this.first_name_txt.Text+"','"+this.last_name_txt.Text+"');"; 
+            MySqlConnection DBconnect = new MySqlConnection(ConnectString);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, DBconnect);
+            MySqlDataReader myReader;
+            try
+            {
+                DBconnect.Open();
+                myReader = cmdDataBase.ExecuteReader();
+               // MessageBox.Show("Added");
+                while (myReader.Read())
+                {
+
+                }
+                refresh();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void refresh()
+        {
+            listBox1.Items.Clear();
+            fill_listbox();
+            btn_delete.Visible = false;
+            btn_delete.Visible = false;
+            first_name_txt.Text = "";
+            last_name_txt.Text = "";
+
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                btn_delete.Visible = true;
+                btn_delete.Visible = true;
+            }
+        }
+       
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            
+            string[] del = listBox1.SelectedItem.ToString().Split(' ');
+           
+            string ConnectString = "Server=localhost;Database=felveteli;Uid=root;Pwd=;";
+            string Query = "delete from people where nr= "+del[0];
+            MySqlConnection DBconnect = new MySqlConnection(ConnectString);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, DBconnect);
+            MySqlDataReader myReader;
+            try
+            {
+                DBconnect.Open();
+                myReader = cmdDataBase.ExecuteReader();
+               // MessageBox.Show("Deleted");
+                while (myReader.Read())
+                {
+
+                }
+                refresh();
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
